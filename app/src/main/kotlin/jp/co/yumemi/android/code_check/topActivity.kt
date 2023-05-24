39875c669ca47b1548388d902874a66b112ea70c
TBSten <81161390+TBSten@users.noreply.github.com>
2023-05-24 22:53:54 +0900

    Composeを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/topActivity.kt
@@ -3,12 +3,23 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
 import java.util.*
 
-class TopActivity : AppCompatActivity(R.layout.activity_top) {
+class TopActivity : ComponentActivity() {
 
     companion object {
         lateinit var lastSearchDate: Date
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("test")
+        }
     }
 }
